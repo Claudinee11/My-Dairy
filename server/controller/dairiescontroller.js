7252785e2e91ce9638af97787b1fdb1f19d46959
Claudinee11 <niyoclaudine10@gmail.com>
2019-09-14 01:35:47 +1400

    [Finishes #168477010] added get entry added delete entry

--- server/controller/dairiescontroller.js
+++ server/controller/dairiescontroller.js
@@ -1,15 +1,15 @@
 
-// import dairies from '../models/dairies';
+import dairies from '../models/dairies';
 
-// const getDairy=(req, res)=> {
-//     res.status(200).send({
-//       success: 'true',
-//       message: 'dairy retrieved successfully',
-//        dairies,
-//     });
-//   };
-
-//   export default getDairy;
+const getDairy=(req, res)=> {
+    res.status(200).send({
+      success: 'true',
+      message: 'dairy retrieved successfully',
+       dairies,
+    });
+  };
+  
+  export default getDairy;
   
 
  
