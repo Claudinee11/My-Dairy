a2e4ea7c8b29761bcb2bf696122e3393a7755856
Claudinee11 <niyoclaudine10@gmail.com>
2019-09-14 00:31:51 +1400

    [Finishes #168364815] added modify endpoint

--- server/controller/dairputcontr.js
+++ server/controller/dairputcontr.js
@@ -1,53 +1,53 @@
 
 
-// import dairies from '../models/dairies';
+import dairies from '../models/dairies';
 
-// //update my dairy
+//update my dairy
 
-//  const putDairy=(req, res)=>{
-//     const id = parseInt(req.params.id, 10);
-//     let dairieFound;
-//     let itemIndex;
-//     dairies.map((dairie, index) => {
-//       if (dairie.id === id) {
-//         dairieFound = dairie;
-//         itemIndex = index;
-//       }
-//     });
+ const putDairy=(req, res)=>{
+    const id = parseInt(req.params.id, 10);
+    let dairieFound;
+    let itemIndex;
+    dairies.map((dairie, index) => {
+      if (dairie.id === id) {
+        dairieFound = dairie;
+        itemIndex = index;
+      }
+    });
   
-//     if (!dairieFound) {
-//       return res.status(404).send({
-//         success: 'false',
-//         message: 'dairy not found',
-//       });
-//     }
+    if (!dairieFound) {
+      return res.status(404).send({
+        success: 'false',
+        message: 'dairy not found',
+      });
+    }
   
-//     if (!req.body.title) {
-//       return res.status(400).send({
-//         success: 'false',
-//         message: 'title is required',
-//       });
-//     } else if (!req.body.description) {
-//       return res.status(400).send({
-//         success: 'false',
-//         message: 'description is required',
-//       });
-//     }
+    if (!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required',
+      });
+    } else if (!req.body.description) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'description is required',
+      });
+    }
   
-//     const updateddairie = {
-//       id: dairieFound.id,
-//       title: req.body.title || dairieFound.title,
-//       description: req.body.description || dairieFound.description,
-//     };
+    const updateddairie = {
+      id: dairieFound.id,
+      title: req.body.title || dairieFound.title,
+      description: req.body.description || dairieFound.description,
+    };
   
-//     dairies.splice(itemIndex, 1, updateddairie);
+    dairies.splice(itemIndex, 1, updateddairie);
   
-//     return res.status(201).send({
-//       success: 'true',
-//       message: 'dairy added successfully',
-//       updateddairie,
-//     });
-//   };
+    return res.status(201).send({
+      success: 'true',
+      message: 'dairy added successfully',
+      updateddairie,
+    });
+  };
 
 
-// export default putDairy;
+export default putDairy;
