44c3398ba34fc49114bc4c50065a0e31628c88e9
Claudinee11 <niyoclaudine10@gmail.com>
2019-09-29 06:57:02 +1400

    [Finishes #168802034] added coveralls modified gitignore modified package json modified readme

---
+++ server/helpers/helpers.js
@@ -0,0 +1,55 @@
+// import joi from 'joi';
+
+// class allvalidation {
+ 
+// validation(newUser){
+// const valUsersigninSchema = {
+// email: Joi.string().email().trim().required(),
+//  password: Joi.string().min(6).max(12).trim().required(),
+//             };
+//  return Joi.validate(newUser, this.valUsersigninschama);
+ 
+//         };
+  
+//  validation(newUser) {
+//  const validesignupSchema = {
+// firstName: Joi.string().min(3).trim().required(),
+// lastName: Joi.string().min(3).trim().required(),
+// email: Joi.string().email({ minDomainAtoms: 2 }).trim().required(),
+// username:joi.string().min(5).trim().required(),
+// password: Joi.string().min(6).max(12).trim().required(),
+//                };
+//             return Joi.validate(newUser, validesignupSchema);
+//           };     
+
+//     validation(postnewDiary) {
+//     const newDairySchema = {
+//     title: Joi.string().required(),
+//     date:joi.string().required(),
+//     description:joi.string().required(),
+//             };
+//             return Joi.validate(postnewDiary, newDairySchema);
+//           };
+
+//  validation(modifyDiary) {
+//  const updateDairySchema = {
+// title: Joi.string().required(),
+// date:joi.string().required(),
+// description:joi.string().required(),
+//                     };
+// return Joi.validate(modifyDiary, updateDairySchema);
+//                   },
+
+// validation(viewDairy){
+//     const viewallDiary ={
+//         title:joi.string().required(),
+//         date:joi.string().required(),
+//         description:joi.string().required(),
+//     };
+//     return joi.validate(viewDairy,viewallDiary);
+// };   
+        
+//         };  
+// export default allvalidation;
+    
+    
