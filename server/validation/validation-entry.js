a6e2c746d559d765c506a64a79f9b1674d54c830
Claudinee11 <niyoclaudine10@gmail.com>
2019-10-03 04:35:17 +1400

    [Finishes #168800392] created a new folder of validation added more test edited controllers modified package json edited eslint

---
+++ server/validation/validation-entry.js
@@ -0,0 +1,14 @@
+import joi from 'joi';
+
+const validateEntry = {
+    validate(diary) {
+        const entries = {
+            title:joi.string().required(),
+            date: joi.string().required(),
+            description: joi.string().required(),
+        };
+        return joi.validate(diary, entries);
+    },
+  };
+  export default validateEntry;
+  
